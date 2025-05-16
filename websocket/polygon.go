cd02505382613628e424072c7bab77cd3d9961dc
santiago guichard <guichardsantiago@gmail.com>
2025-05-16 12:45:37 -0300

    updated imports with new module name

--- websocket/polygon.go
+++ websocket/polygon.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/cenkalti/backoff/v4"
 	"github.com/gorilla/websocket"
-	"github.com/polygon-io/client-go/websocket/models"
+	"github.com/Guichardsantiago/polygon-client-go/websocket/models"
 	"golang.org/x/exp/maps"
 	"golang.org/x/exp/slices"
 	"gopkg.in/tomb.v2"
